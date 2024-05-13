90f8c9a90432deb12433623e6992d89bf3b620b8
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-13 17:29:57 +0200

    creata variabile array contenente mail utenti che sono presenti

--- Chiedi la mail/js/mail.js
+++ Chiedi la mail/js/mail.js
@@ -1,1 +1,4 @@
 const userEmail = prompt('scrivi la tua mail');
+
+
+const userList = ['giovannirossi@mail.com', 'alessandrofumagalli@mail.com', 'ginoginetti@mail.com', 'robrobotto@mail.com', 'pincopallo@mail.com', 'tiziocaio@mail.com'];
