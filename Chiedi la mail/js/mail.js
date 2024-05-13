b4f8a295aeaa8d04b249c81d7bc8f5495f386db9
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-13 17:31:24 +0200

    create condizioni in cui si verifica la presenza dell utente nel array, e di conseguenza si garantisce o nega l'accesso

--- Chiedi la mail/js/mail.js
+++ Chiedi la mail/js/mail.js
@@ -2,3 +2,10 @@ const userEmail = prompt('scrivi la tua mail');
 
 
 const userList = ['giovannirossi@mail.com', 'alessandrofumagalli@mail.com', 'ginoginetti@mail.com', 'robrobotto@mail.com', 'pincopallo@mail.com', 'tiziocaio@mail.com'];
+
+if (userList.includes(userEmail)) {
+    console.log('accesso consentito');
+
+} else {
+    console.log('accesso negato');
+}
