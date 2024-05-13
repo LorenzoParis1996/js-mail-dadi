7247d9faee2a0251a5b61ece4f193121525b423c
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-13 17:26:26 +0200

    create condizioni di vincita e parità

--- Gioco dei dadi/js/dadi.js
+++ Gioco dei dadi/js/dadi.js
@@ -2,4 +2,13 @@ alert('il dado verrà lanciato. chi vincerà?');
 const humanPlayer = Math.floor(Math.random() * 6 + 1, 10);
 const aiPlayer = Math.floor(Math.random() * 6 + 1, 10);
 
-for (let diceFace = 1; diceFace <= 6; diceFace++);
+for (let diceFace = 1; diceFace <= 6; diceFace++) {
+
+    if (humanPlayer > aiPlayer) {
+        console.log(humanPlayer, aiPlayer, 'vince l umano');
+    } else if (humanPlayer < aiPlayer) {
+        console.log(humanPlayer, aiPlayer, 'vince la macchina');
+    } else {
+        console.log(humanPlayer, aiPlayer, 'siete pari');
+    }
+}
