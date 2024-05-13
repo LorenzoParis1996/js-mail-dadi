c3fa32f459006a3a1b18ddda3a58344b83bfd82e
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-13 17:25:21 +0200

    creato ciclo for per iterazione da 1 a 6

--- Gioco dei dadi/js/dadi.js
+++ Gioco dei dadi/js/dadi.js
@@ -2,3 +2,4 @@ alert('il dado verrà lanciato. chi vincerà?');
 const humanPlayer = Math.floor(Math.random() * 6 + 1, 10);
 const aiPlayer = Math.floor(Math.random() * 6 + 1, 10);
 
+for (let diceFace = 1; diceFace <= 6; diceFace++);
